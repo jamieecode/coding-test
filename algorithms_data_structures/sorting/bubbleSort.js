87a2213efbfc244ac09ab64f1a8cc5dae8f69266
Jaewan <jamieecode@gmail.com>
2021-11-27 23:45:47 +0900

    bubblesort

---
+++ algorithms_data_structures/sorting/bubbleSort.js
@@ -0,0 +1,36 @@
+// JavaScript has "sort" method but it does NOT work with numbers (because sort method sorts in Unicode order)
+// However, this method accepts "comparator function". By using this, you can compare elements
+// and sort order based on the return value.
+// if it returns a negative number, its order will be a to b
+// if it returns a positive number, its order will be b to a
+// .sort((a,b) => a-b) OR .sort((a,b) => b-a)
+
+// REMEMBER swap!!
+// let temp = array[i]
+// array[i] = array[j]
+// array[j] = temp
+
+// BubbleSort
+// largest value goes up to the top
+
+// BIG O - worst case => O(n^2)
+//       - best case => O(n)
+
+function bubbleSort(array) {
+  let isSwapped;
+
+  for (let i = array.length; i > 0; i--) {
+    isSwapped = false;
+
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        isSwapped = true;
+      }
+    }
+    if (!isSwapped) break;
+  }
+  return array;
+}

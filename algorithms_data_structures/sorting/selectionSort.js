da065f241fc7264a7499131bae63d8821ab075b4
Jaewan <jamieecode@gmail.com>
2021-11-28 22:02:59 +0900

    selection sort

---
+++ algorithms_data_structures/sorting/selectionSort.js
@@ -0,0 +1,24 @@
+// Set first element as the smallest value.
+// Compare this with the next item in the array until you find a smaller one.
+// When a smaller number is found, it is going to be the new smallest value til the end of the array.
+// If the index of smallest value is not the one you set in the beginning, swap two values.
+
+// BIG O - worst case => O(n^2)
+//       - best case => O(n^2)
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let smallestIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[smallestIndex] > array[j]) {
+        smallestIndex = j;
+      }
+    }
+    if (i !== smallestIndex) {
+      let temp = array[i];
+      array[i] = array[smallestIndex];
+      array[smallestIndex] = temp;
+    }
+  }
+  return array;
+}

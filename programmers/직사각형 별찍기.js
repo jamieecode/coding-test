8afc8726c35f1dacd8f106154d0e09fd2f1c3c3c
Jaewan <jamieecode@gmail.com>
2022-01-27 23:09:20 +0900

    직사각형 별찍기

---
+++ programmers/직사각형 별찍기.js
@@ -0,0 +1,15 @@
+// 프로그래머스 > 연습문제 > 직사각형 별찍기
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  for (let i = 0; i < b; i++) {
+    let answer = "";
+    for (let j = 0; j < a; j++) {
+      answer += "*";
+    }
+    console.log(answer);
+  }
+});
